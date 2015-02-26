93bea89963df6b639e7ac5ee86927e022f35187c
PhelanBavaria <antextides@gmail.com>
2015-02-26 17:31:35 +0100

    Now draws borders on provinces

--- common/widgets/overlays/political.py
+++ common/widgets/overlays/political.py
@@ -1,6 +1,7 @@
 
 
 import random
+from operator import itemgetter
 from common.widgets.overlays import Overlay
 
 
@@ -8,6 +9,7 @@ class Political(Overlay):
     def __init__(self, surface):
         Overlay.__init__(self, surface)
         self.unit_locations = {}
+        self.borders = {}
 
     def update(self, province, area):
         bcolor = ()
@@ -15,13 +17,14 @@ class Political(Overlay):
             pcolor = (90, 90, 90)
         elif province.water:
             pcolor = (64, 64, 157)
+            bcolor = (100, 100, 157)
         elif not province.controller:
             pcolor = (160, 160, 160)
         else:
             pcolor = province.controller.pcolor
             bcolor = province.controller.bcolor
         if not bcolor:
-            bcolor = (255-v for v in pcolor)
+            bcolor = tuple(255-v for v in pcolor)
 
         if province.color not in self.unit_locations.keys():
             self.unit_locations[province.color] = []
@@ -35,7 +38,35 @@ class Political(Overlay):
                 pos = random.choice(self.unit_locations[province.color])
                 self.unit_locations[province.color].remove(pos)
 
+        if province.color not in self.borders.keys():
+            border = set()
+            sorted_area = sorted(area)
+            last_x, last_y = sorted_area[0]
+            for x, y in sorted_area[1:]:
+                #print('last_x', last_x, 'last_y', last_y)
+                #input('(x, y): (' + str(x) + ', ' + str(y) + ')')
+                #print(x > last_x+1, y != last_y)
+                if y > last_y+1 or x != last_x:
+                    border.add((x, y))
+                    border.add((last_x, last_y))
+                last_x, last_y = x, y
+            sorted_area = sorted(area, key=itemgetter(1, 0))
+            last_x, last_y = sorted_area[0]
+            for x, y in sorted_area[1:]:
+                #print('last_x', last_x, 'last_y', last_y)
+                #input('(x, y): (' + str(x) + ', ' + str(y) + ')')
+                #print(x > last_x+1, y != last_y)
+                if x > last_x+1 or y != last_y:
+                    border.add((x, y))
+                    border.add((last_x, last_y))
+                last_x, last_y = x, y
+
+            self.borders[province.color] = border
+
         for pixel in area:
             self.surface.set_at(pixel, pcolor)
         for pixel in self.unit_locations[province.color]:
+            self.surface.set_at(pixel, bcolor)
+        for pixel in self.borders[province.color]:
+            #print(pixel, bcolor)
             self.surface.set_at(pixel, bcolor)
